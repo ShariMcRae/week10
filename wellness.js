873c539dce5b5b7e8e0f22636e1b9621bd4a1103
Shari McRae <sharimcrae@protonmail.com>
2023-02-22 09:45:33 -0600

    added comments and changed style of table

--- wellness.js
+++ wellness.js
@@ -1,6 +1,45 @@
+// create counter to uniquely identify each row as they are created
 let id = 0;
 
+// for convenience, set variables holding form and 'list' table body elements
+const form = document.querySelector("form");
+const table = document.querySelector("#list tbody");
+
+// Add 'submit' event listener to form that preventss default
+// response and adds a new row to the 'list' table.
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  // Create string holding HTML for new row with current 'id'
+  // counter in the id attribute of the row (tr) and button cell (td)
+  // so we can retrieve those DOM elements later by ID.
+  let newRow = `
+        <tr id="row-${id}">
+        <td>${e.target.date.value}</td>
+        <td>${e.target.slept.value}</td>
+        <td>${e.target.energy.value}</td>
+        <td id="button-cell-${id}"></td>
+        </tr>
+    `;
+
+  // Insert HTML for new row at end of table body element
+  table.insertAdjacentHTML("beforeend", newRow);
+
+  // Get td element that will hold the delete button,
+  // create new delete button, insert button into td element,
+  // and increment 'id' counter.
+  let buttonCell = document.getElementById("button-cell-" + id);
+  buttonCell.appendChild(createDeleteButton(id++));
+
+  // clear form fields
+  e.target.reset();
+});
+
+// For a given a row id (id) create and return a new Delete button.
+// Set onclick event to call function that will remove the row with id `row-${id}`
 function createDeleteButton(id) {
+
   let btn = document.createElement("button");
   btn.className = "btn btn-primary btn-sm";
   btn.innerHTML = "Delete";
@@ -10,33 +49,3 @@ function createDeleteButton(id) {
   };
   return btn;
 }
-
-const form = document.querySelector("form");
-const table = document.querySelector("#list tbody");
-
-form.addEventListener("submit", (e) => {
-
-  e.preventDefault();
-  const date = e.target.date.value;
-  const slept = e.target.slept.value;
-  const energy = e.target.energy.value;
-
-  if ((date === "") || (slept === "") || (energy === ""))
-    alert("All three fields are required.");
-  else {
-    let newRow = `
-        <tr id="row-${id}">
-        <td>${date}</td>
-        <td>${slept}</td>
-        <td>${energy}</td>
-        <td id="button-cell-${id}"></td>
-        </tr>
-    `;
-
-    table.insertAdjacentHTML("beforeend", newRow);
-    let buttonCell = document.getElementById("button-cell-" + id);
-    buttonCell.appendChild(createDeleteButton(id++));
-    e.target.reset();
-  }
-
-});
